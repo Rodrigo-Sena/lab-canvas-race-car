a6190d09acd7e230bfa26da60336cf6bc8ee156f
Rodrigo-Sena <sena.rodrigo@outlook.com>
2020-06-12 21:46:13 +0100

    add lines for letters

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
 window.onload = () => {
+
+  const myGameArea = {
+    canvas: document.getElementById('canvas'),
+    context: this.canvas.getContext('2d'),
+    drawRoad: function() {
+    const roadImg = new Image();
+    roadImg.src = './images/road.png';
+    const that = this
+    roadImg.onload = function(){
+      
+      that        .context.drawImage(roadImg, 0,0, 500,700)
+      }
+   
+    }
+  }
+
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  myGameArea.drawRoad()
+
+  function startGame() {
+    console.log ('yo!')
+    
+  }
+
+
 };
